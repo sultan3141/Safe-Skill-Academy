706ac10b65e04e0f774241a8b6d8d47783cb190f
Sultan Adinan <Adinanyous1@gmail.com>
2025-09-29 15:14:32 +0000

    second

--- Safe_Skill_Academy/Safe_Skill_Academy/settings.py
+++ Safe_Skill_Academy/Safe_Skill_Academy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,16 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+   
     "core",
     "userauths",
     "api",
-    "students",
+    "students",   
 ]
 
 MIDDLEWARE = [
@@ -58,7 +61,7 @@ ROOT_URLCONF = "Safe_Skill_Academy.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,7 +123,20 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
+
+STATIC_ROOT=BASE_DIR/'templates'
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Library Admin",
+    "welcome_sign": "Welcome to the library",
+    "show_ui_builder": True,
+    }
