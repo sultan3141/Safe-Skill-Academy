5c282de8e996a91599fd450f2a2dc6f15bbe2a52
Sultan Adinan <Adinanyous1@gmail.com>
2025-10-02 14:35:26 +0000

    frist

--- Safe_Skill_Academy/api/urls.py
+++ Safe_Skill_Academy/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/register/', api_views.RegisterView.as_view(), name='register'),
     path('user/password-reset-email/<email>/', api_views.PasswordRestEmailVerifyView.as_view(), name='password_reset_email'),
+    path('user/password-change/', api_views.PasswordChangeView.as_view(), name='change_password'),
 ]
--- Safe_Skill_Academy/api/views.py
+++ Safe_Skill_Academy/api/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from api import serializer as api_serializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from userauths.models import User, Profile
-from rest_framework import generics
+from rest_framework import generics, response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenObtainPairSerializer
@@ -35,6 +37,24 @@ class PasswordRestEmailVerifyView(generics.GenericAPIView):
             user.otp=generate_random_otp()
             user.save()
 
-            link=f"http://localhost:5173/create-new-password/?otp{user.otp}&uuidb64={uuid64}/"
+            link=f"http://localhost:5173/create-new-password/?otp={user.otp}&uuidb64={uuid64}=refresh_token{refresh_token}"
             print("link======",link)
          return user   
+class PasswordChangeView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        uuidb64=request.data['uuidb64']
+        otp=request.data['otp']
+        password=request.data['password']
+        
+        user=User.objects.get(id=uuidb64,otp=otp)
+        if user:
+           user.set_password(password)
+           user.otp=''
+           user.save()
+
+           return Response({"message": "Password changed successfully"}, status=status.HTTP_201_CREATED)
+        else:
+           return Response({"message": "User Does Not Exists"}, status=status.HTTP_400_BAD_REQUEST)
